4e614391c6177c92b595edafaa997203791e2ff6
Codbas <cody.bassler@gmail.com>
2024-06-03 14:11:30 -0700

    updated errors for missing games and devlogs

--- src/public/DevLog.php
+++ src/public/DevLog.php
@@ -3,15 +3,35 @@
 /* @var PDO $dbConn
 */
 
-$title = urldecode($_GET['title']);
-$title = preg_replace('/[^a-zA-Z0-9 .,\-!?]/', '', $title);
+if (!isset($_GET['title'])) {
+    $title = 'Dev Log';
+}
+else {
+    $title = urldecode($_GET['title']);
+    $title = preg_replace('/[^a-zA-Z0-9 .,\-!?]/', '', $title);
+}
 require_once '../includes/config.php';
 
 echo '<body>';
 
-// TODO : if no devlog found, display error message and stop executing script
+$devlogsDir = '../../devlogs';
+$devlogExists = true;
+
+if (!is_dir($devlogsDir)) {
+    $devlogExists = false;
+}
+if ($devlogExists) {
+    $contents = scandir($devlogsDir);
+    $folders = array_diff($contents, ['.', '..', '.DS_Store']);
+    if (!in_array($title, $folders)) {
+        $devlogExists = false;
+    }
+}
 
 // TODO: increment dev log view count
+if (!$devlogExists) {
+    exit('<p style="margin-top: 10%; font-size: 20px; text-align: center;">ERROR: ' . $title . ' does not exist</p>');
+}
 
 $devlog = new DevLogLoader($title, $dbConn);
 
--- src/public/Game.php
+++ src/public/Game.php
@@ -4,11 +4,35 @@
  */
 // Retrieved with GET, the game to load is under the parameter "game"
 
-$title = urldecode($_GET['title']);
-$title = preg_replace('/[^a-zA-Z0-9 .,\-!?]/', '', $title);
+if (!isset($_GET['title'])) {
+    $title = 'Game';
+}
+else {
+    $title = urldecode($_GET['title']);
+    $title = preg_replace('/[^a-zA-Z0-9 .,\-!?]/', '', $title);
+}
+
 require_once '../includes/config.php';
 
 echo '<body>';
+
+$gamesDir = '../../games';
+$gameExists = true;
+
+if (!is_dir($gamesDir)) {
+    $devlogExists = false;
+}
+if ($gameExists) {
+    $contents = scandir($gamesDir);
+    $folders = array_diff($contents, ['.', '..', '.DS_Store']);
+    if (!in_array($title, $folders)) {
+        $gameExists = false;
+    }
+}
+
+if (!$gameExists) {
+    exit('<p style="margin-top: 10%; font-size: 20px; text-align: center;">ERROR: ' . $title . ' does not exist</p>');
+}
 
 $game = new GameLoader($title, $dbConn);
 
